891f4924a32fb846911bb8620fa76a91e006f1f8
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-12-05 11:23:41 -0300

    x

--- cos.tf
+++ cos.tf
@@ -17,12 +17,3 @@ resource "ibm_cos_bucket" "ontap_image" {
   storage_class        = "standard"
   force_delete         = true
 }
-
-resource "ibm_cos_bucket_object" "ontap_image_object" {
-  count      = var.upload_local_image ? 1 : 0
-  bucket_crn = ibm_cos_bucket.ontap_image.crn
-  key        = var.ontap_image_object_key
-  file_path  = var.ontap_image_file
-  etag       = var.upload_local_image ? filemd5(var.ontap_image_file) : null
-  bucket_location = var.region
-}
