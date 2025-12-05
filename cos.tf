ed8f30d921686de411c8e4b0c411161e9f04baff
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-12-05 12:18:14 -0300

    x

--- cos.tf
+++ cos.tf
@@ -8,7 +8,7 @@ resource "ibm_resource_instance" "cos" {
 
 resource "ibm_cos_bucket" "ontap_image" {
   bucket_name          = var.cos_bucket_name
-  resource_instance_id = ibm_resource_instance.cos.guid
+  resource_instance_id = ibm_resource_instance.cos.crn
   region_location      = var.region
   storage_class        = "standard"
   force_delete         = true
